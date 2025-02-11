10de222950d496100572c383efd3e59070340dcd
usman-369 <m.usman.8460@gmail.com>
2025-02-11 17:09:19 +0500

    ditched for now

--- ditched.py
+++ ditched.py
@@ -33,4 +33,6 @@
 #     print("\n\tNote: Please use ")
 #     wlk_duration = input("\n\tEnter the duration of walk/daycare (e.g. 60-minutes/24-hours): ").strip().lower().split("-")
 
-#     if (len(wlk_duration) != 2)
+#     if (len(wlk_duration) != 2) or (not wlk_duration[0].isdigit()) or (not wlk_duration[1].isalpha()):
+#         print("\n\tInvalid Format! Try again. :0")
+#         continue
